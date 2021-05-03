b5968fbe82323cda129200523beb70df0980fd74
benalder <58360925+karolkecinski@users.noreply.github.com>
2021-05-03 13:48:01 +0200

    Added Project 1

---
+++ Unity 3D Course/Project 1/Project 1/Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 1000f;
+    public float sidewaysForce = 500f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        float x = GetAxis("Horizontal");
+
+        if(x > 0.0)
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if(x < 0.0)
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
