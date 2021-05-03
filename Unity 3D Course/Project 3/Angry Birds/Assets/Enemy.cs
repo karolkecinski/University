c8a8a782840d88c2350a8e74d6742b7d2f3d5612
benalder <58360925+karolkecinski@users.noreply.github.com>
2021-05-03 14:08:17 +0200

    Added Project 3

---
+++ Unity 3D Course/Project 3/Angry Birds/Assets/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D colInfo)
+    {
+        if(colInfo.collider.name == "Ground")
+        {
+            endgame();
+        }
+    }
+
+    void endgame()
+    {
+        Destroy(gameObject);
+    }
+}
