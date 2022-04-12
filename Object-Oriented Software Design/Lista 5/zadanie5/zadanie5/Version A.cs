40341429025c7149d07a9b3c593373c5283e2df5
karolkecinski <58360925+karolkecinski@users.noreply.github.com>
2022-04-12 21:17:50 +0200

    Added Solution for Set 5

---
+++ Object-Oriented Software Design/Lista 5/zadanie5/zadanie5/Version A.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie5
+{
+    public class Person
+    {
+        public Person()
+        {
+
+        }
+
+    }
+    public interface IPersonNotifier
+    {
+        public void NotifyPersons(IEnumerable<Person> persons);
+    }
+    public abstract class PersonRegistry
+    {
+        IPersonNotifier _notifier;
+        public PersonRegistry(IPersonNotifier notifier)
+        {
+            this._notifier = notifier;
+        }
+        public abstract IEnumerable<Person> GetPersons();
+
+        public void NotifyPersons()
+        {
+            this._notifier.NotifyPersons(this.GetPersons());
+        }
+    }
+
+    public class XMLPersonRegistry : PersonRegistry
+    {
+        public XMLPersonRegistry(IPersonNotifier notifier) : base(notifier)
+        {
+
+        }
+
+        public override IEnumerable<Person> GetPersons()
+        {
+            return new List<Person>() { new Person() };
+        }
+    }
+
+    public class SQLPersonRegistry : PersonRegistry
+    {
+        public SQLPersonRegistry(IPersonNotifier notifier) : base(notifier)
+        {
+
+        }
+
+        public override IEnumerable<Person> GetPersons()
+        {
+            return new List<Person>() { new Person() };
+        }
+    }
+
+    public class MailPersonNotifier : IPersonNotifier
+    {
+        public void NotifyPersons(IEnumerable<Person> persons)
+        {
+            foreach (Person p in persons)
+            {
+                Console.WriteLine("Notified person");
+            }
+        }
+    }
+
+    public class SMSPersonNotifier : IPersonNotifier
+    {
+        public void NotifyPersons(IEnumerable<Person> persons)
+        {
+            foreach (Person p in persons)
+            {
+                Console.WriteLine("Notified person");
+            }
+        }
+    }
+}
---
+++ Object-Oriented Software Design/Lista 5/zadanie5/zadanie5/Version B.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie5
+{
+    public interface IPersonsGetter
+    {
+        public IEnumerable<Person> GetPersons();
+    }
+    public abstract class PersonRegistry2
+    {
+        IPersonsGetter _getter;
+        public PersonRegistry2(IPersonsGetter getter)
+        {
+            this._getter = getter;
+        }
+        public IEnumerable<Person> GetPersons()
+        {
+            return this._getter.GetPersons();
+        }
+
+        public abstract void NotifyPersons(IEnumerable<Person> persons);
+    }
+
+    public class MailPersonNotifier2 : PersonRegistry2
+    {
+        public MailPersonNotifier2(IPersonsGetter getter) : base(getter)
+        {
+
+        }
+
+        public override void NotifyPersons(IEnumerable<Person> persons)
+        {
+            foreach (Person p in persons)
+            {
+                Console.WriteLine("Notified person");
+            }
+        }
+    }
+
+    public class SMSPersonNotifier2 : PersonRegistry2
+    {
+        public SMSPersonNotifier2(IPersonsGetter getter) : base(getter)
+        {
+
+        }
+
+        public override void NotifyPersons(IEnumerable<Person> persons)
+        {
+            foreach (Person p in persons)
+            {
+                Console.WriteLine("Notified person");
+            }
+        }
+    }
+
+    public class XMLPersonGetter : IPersonsGetter
+    {
+
+        public IEnumerable<Person> GetPersons()
+        {
+            return new List<Person>() { new Person() };
+        }
+    }
+
+    public class SQLPersonGetter : IPersonsGetter
+    {
+
+        public IEnumerable<Person> GetPersons()
+        {
+            return new List<Person>() { new Person() };
+        }
+    }
+
+    
+}
