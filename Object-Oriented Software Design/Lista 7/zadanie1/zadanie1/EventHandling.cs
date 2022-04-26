e9795acde19d96cbdba0819bff7f67ef38b53895
karolkecinski <58360925+karolkecinski@users.noreply.github.com>
2022-04-26 12:08:38 +0200

    Commit to Set 7 exercise 1

--- Object-Oriented Software Design/Lista 7/zadanie1/zadanie1/EventHandling.cs
+++ Object-Oriented Software Design/Lista 7/zadanie1/zadanie1/EventHandling.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace zadanie1
+﻿namespace EventHandling
 {
-    internal class Class1
+    public interface ISubscriber<T>
     {
+        void Handle(T notification);
+    }
+
+    public interface IEventAggregator
+    {
+        void AddSubscriber<T>(ISubscriber<T> subscriber);
+        void RemoveSubscriber<T>(ISubscriber<T> subscriber);
+        void Publish<T>(T Event);
+    }
+
+    public class EventAggregator : IEventAggregator
+    {
+        Dictionary<Type, List<object>> _subscribers =
+            new Dictionary<Type, List<object>>();
+
+        #region IEventAggregator Members
+
+        public void AddSubscriber<T>(ISubscriber<T> subscriber)
+        {
+            if (!_subscribers.ContainsKey(typeof(T)))
+                _subscribers.Add(typeof(T), new List<object>());
+
+            _subscribers[typeof(T)].Add(subscriber);
+        }
+
+        public void RemoveSubscriber<T>(ISubscriber<T> subscriber)
+        {
+            if (!_subscribers.ContainsKey(typeof(T)))
+                _subscribers.[typeof(T)].Remove(subscriber);
+        }
+
+        public void Publish<T>(T Event)
+        {
+            if (_subscribers.ContainsKey(typeof(T)))
+            {
+                foreach (ISubscriber<T> subscriber in _subscribers[typeof(T)].OfType<ISubscriber<T>>())
+                    subscriber.Handle(Event);
+            }
+        }
+
+        #endregion
     }
 }
---
+++ Object-Oriented Software Design/Lista 7/zadanie1/zadanie1/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EventHandling;
+using Notifications;
+
+namespace Person
+{
+    public abstract class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Id { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class Student : Person
+    {
+
+    }
+
+    public class Lecturer : Person
+    {
+
+    }
+}
