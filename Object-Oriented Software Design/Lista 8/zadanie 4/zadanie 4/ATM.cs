a4592a4a0ede1552806ecbe66a7b1e1ab2f75d11
karolkecinski <58360925+karolkecinski@users.noreply.github.com>
2022-05-05 20:28:12 +0200

    Updated Lista 8

---
+++ Object-Oriented Software Design/Lista 8/zadanie 4/zadanie 4/ATM.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using State;
+
+namespace Atm
+{
+    public class ATM
+    {
+        public IState state;
+
+        public ATM()
+        {
+        }
+
+        public void SetState(IState state)
+        {
+            this.state = state;
+        }
+
+        public void ProcessCashlessTransaction()
+        {
+            this.state.ProcessCashlessTransaction();
+        }
+        public void ProcessTransactionWithCash()
+        {
+            this.state.ProcessTransactionWithCash();
+        }
+
+        public void FinalizeTransaction()
+        {
+            this.state.FinalizeTransaction();
+        }
+    }
+}
---
+++ Object-Oriented Software Design/Lista 8/zadanie 4/zadanie 4/State.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Atm;
+
+namespace State
+{
+    public interface IState
+    {
+        void ProcessCashlessTransaction();
+        void ProcessTransactionWithCash();
+        void FinalizeTransaction();
+    }
+
+    public class IdleState : IState
+    {
+        private ATM atm;
+
+        public IdleState(ATM atm)
+        {
+            this.atm = atm;
+        }
+
+        public void ProcessCashlessTransaction() { throw new Exception(); }
+
+        public void ProcessTransactionWithCash() { throw new Exception(); }
+
+        public void FinalizeTransaction()
+        {
+            Console.WriteLine("Transaction successfull!");
+        }
+    }
+
+    public class CashlessTransactionState : IState
+    {
+        private ATM atm;
+
+        public CashlessTransactionState(ATM atm)
+        {
+            this.atm = atm;
+        }
+
+        public void ProcessCashlessTransaction()
+        {
+            Console.WriteLine("Processing cashless transaction...");
+            this.atm.state = new IdleState(this.atm);
+        }
+
+        public void ProcessTransactionWithCash()
+        {
+            throw new Exception();
+        }
+
+        public void FinalizeTransaction()
+        {
+            throw new Exception();
+        }
+    }
+
+    public class CashTransactionState : IState
+    {
+        private ATM atm;
+
+        public CashTransactionState(ATM atm)
+        {
+            this.atm = atm;
+        }
+
+        public void ProcessCashlessTransaction()
+        {
+            throw new Exception();
+        }
+
+        public void ProcessTransactionWithCash()
+        {
+            Console.WriteLine("Processing transaction with cash...");
+            this.atm.state = new IdleState(this.atm);
+        }
+
+        public void FinalizeTransaction()
+        {
+            throw new Exception();
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ATM atm1 = new ATM();
+
+            atm1.SetState(new CashlessTransactionState(atm1));
+            atm1.ProcessCashlessTransaction();
+            atm1.FinalizeTransaction();
+
+            ATM atm2 = new ATM();
+
+            atm2.SetState(new CashTransactionState(atm2));
+            atm2.ProcessTransactionWithCash();
+            atm2.FinalizeTransaction();
+
+            try
+            {
+                ATM atm3 = new ATM();
+
+                atm3.SetState(new CashlessTransactionState(atm3));
+                atm3.FinalizeTransaction();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
